f83b307570083d486f5519d79037d1f5f564dc14
Amakazor <amakazor@gmail.com>
2019-10-03 23:58:22 +0200

    header file containing other header files

---
+++ console-tank-game/console-tank-game/incude.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <windows.h> 
+#include <iostream>
+#include <conio.h>
+#include <filesystem>
+#include "arek.h"
+
+namespace fs = std::filesystem;
